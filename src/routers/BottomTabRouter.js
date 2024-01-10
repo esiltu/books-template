1c973af967d6421928ddca05a1ebee1bbe3d5933
E Siltu <machinevaneray@192.168.68.103>
2024-01-10 20:31:16 +0100

    Wed Jan 10 20:31:16 CET 2024 Working on Auth -> Tab pages

---
+++ src/routers/BottomTabRouter.js
@@ -0,0 +1,4 @@
+export { default as Cart } from "../pages/Tab/Cart";
+export { default as Category } from "../pages/Tab/Category";
+export { default as Home } from "../pages/Tab/Home";
+export { default as Profile } from "../pages/Tab/Home";
